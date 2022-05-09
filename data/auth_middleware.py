a9d01dc7a0cf841ba559ef886c1d626334cc994a
RyanMouraBrocco <ryanmourabrocco@gmail.com>
2022-05-08 23:25:59 -0300

    added data_server features and lock by index

---
+++ data/auth_middleware.py
@@ -0,0 +1,24 @@
+from http.client import UNAUTHORIZED
+from os import abort
+from werkzeug.wrappers import Request, Response, ResponseStream
+
+
+class authMiddleware():
+
+    def __init__(self, app):
+        self.app = app
+        self.serverAuth = [
+            "SNDFISFNEI82FH28FN392HFBR3289FH32FN2FJ3290",
+            "PDIOFNSUIDCN298904Y3FBSBV87DW0GCSUB78GW178",
+            "FASNOFSI81D01BUABCS7Q8S087ACGSABCS7AC80AGC"
+        ]
+
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+        authKey = request.headers['auth']
+
+        if authKey in self.serverAuth:
+            return self.app(environ, start_response)
+
+        unauthorizedResponse = Response(u'Unauthorized', mimetype= 'text/plain', status=401)
+        return unauthorizedResponse(environ, start_response)
--- data/data_server.py
+++ data/data_server.py
@@ -1,58 +1,111 @@
-from flask import Flask
+from threading import Lock
+from os import unlink
+from flask import Flask, abort, request
+from auth_middleware import authMiddleware
+
 app = Flask(__name__)
 
-
-# here for a simplify the implementation i will use a database lock in whole table always and not per index
+# this object is like a database with tables, then it has the lock in whole table and lock by id index
+# in this case lock in table is just used to add new value, i can edit some value even the table is in lock
 database = {
     "accounts": {
-        "data": {
-            2: {
-                "data": 200,
-                "state": {"locked": False, "owner": 1}
-            }
-        },
+        "data": {},
         "lock": Lock()
     }
 }
 
-
-def getOrAddAccountInDictionary(businessId, accountId):
-    database["accounts"]["lock"].acquire()
-
-    if(database["accounts"]["data"][accountId] == None):
-        database["accounts"][accountId] = {
-            data: 0, state: {locked: False, owner: businessId}}
-
-    database["accounts"]["lock"].release()
-
-    return database["accounts"][accountId]
+app.wsgi_app = authMiddleware(app.wsgi_app)
 
 
-@app.route("/")
+def lockTable(tableName):
+    database[tableName]["lock"].acquire()
+
+
+def unLockTable(tableName):
+    database[tableName]["lock"].release()
+
+
+def lockItemInTable(tableName, id):
+    database[tableName]["data"][id]["lock"].acquire()
+
+
+def unLockItemInTable(tableName, id):
+    database[tableName]["data"][id]["lock"].release()
+
+
+def getValueInTableById(tableName, id):
+    return database[tableName]["data"][id]
+
+
+def createIfNotExistsItemInAccount(businessId, accountId):
+    lockTable('accounts')
+
+    if(not accountId in database["accounts"]["data"].keys()):
+        database["accounts"]["data"][accountId] = {
+            "amount": 0,
+            "state": {"locked": False, "owner": businessId},
+            "lock": Lock()
+        }
+
+    unLockTable('accounts')
+
+
+@app.route("/getLock/<int:businessId>/<int:accountId>", methods=['POST'])
 def getLock(businessId, accountId):
-    currentItem = getOrAddAccountInDictionary(businessId, accountId)
-    if(currentItem.state.locked)
-    return -1
+    createIfNotExistsItemInAccount(businessId, accountId)
+    lockItemInTable('accounts', accountId)
 
-    currentItem.state.locked = True
+    account = getValueInTableById('accounts', accountId)
+    if(account["state"]["locked"]):
+        unLockItemInTable('accounts', accountId)
+        return ("-1", 401)
+
+    account["state"]["locked"] = True
+    account["state"]["owner"] = businessId
+    unLockItemInTable('accounts', accountId)
+    return ('', 204)
 
 
-@app.route("/")
+@app.route("/unLock/<int:businessId>/<int:accountId>", methods=['POST'])
 def unLock(businessId, accountId):
-    currentItem = getOrAddAccountInDictionary(businessId, accountId)
-    if(currentItem.state.locked and currentItem.state.owner != businessId)
-    return -1
+    createIfNotExistsItemInAccount(businessId, accountId)
 
-    currentItem.state.locked = False
+    lockItemInTable('accounts', accountId)
+    account = getValueInTableById('accounts', accountId)
+    if(account["state"]["locked"] and account["state"]["owner"] != businessId):
+        unLockItemInTable('accounts', accountId)
+        return ("-1", 401)
+
+    account["state"]["locked"] = False
+    unLockItemInTable('accounts', accountId)
+    return ('', 204)
 
 
-@app.route("/")
-def setAmount(businessId, accountId):
-    currentItem = getOrAddAccountInDictionary(businessId, accountId)
-    if(currentItem.state.locked and currentItem.state.owner != businessId)
-    return -1
+@ app.route("/setAmount/<int:businessId>/<int:accountId>/<amount>", methods=['POST'])
+def setAmount(businessId, accountId, amount):
+    amount = float(amount)
+    createIfNotExistsItemInAccount(businessId, accountId)
 
-    currentItem.state.locked = False
+    lockItemInTable('accounts', accountId)
+    account = getValueInTableById('accounts', accountId)
+    if(not account["state"]["locked"] or account["state"]["owner"] != businessId):
+        unLockItemInTable('accounts', accountId)
+        return ("-1", 401)
+
+    account["amount"] = amount
+    unLockItemInTable('accounts', accountId)
+    return ('', 204)
+
+
+@ app.route("/getAmount/<int:businessId>/<int:accountId>", methods=['GET'])
+def getAmount(businessId, accountId):
+    createIfNotExistsItemInAccount(businessId, accountId)
+
+    account = getValueInTableById('accounts', accountId)
+    if(account["state"]["locked"] and account["state"]["owner"] != businessId):
+        return ("-1", 401)
+
+    return str(account["amount"])
 
 
 if __name__ == "__main__":
