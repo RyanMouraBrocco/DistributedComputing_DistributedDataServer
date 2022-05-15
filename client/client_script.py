3d221b3a0a19a121598e95e9e8010fc6bb18ea12
RyanMouraBrocco <ryanmourabrocco@gmail.com>
2022-05-15 00:28:03 -0300

    fix bugs in tests

--- client/client_script.py
+++ client/client_script.py
@@ -13,7 +13,7 @@ def printTestResult(test, accountId, succeessfulMessage):
 
 
 def tryRequestWithoutAuthenticaiton(accountId):
-    bankRepository = BankRepository()
+    bankRepository = BankRepository("")
     try:
         bankRepository.deposit(accountId, 10)
     except:
@@ -31,7 +31,7 @@ def checkIfOnlyIn5ActionSaveInformations(accountId):
         if(i != 4 and currentAmout != initialAmount):
             raise Exception(
                 '[fail]: in less than 5 actions the values was changed')
-        elif(currentAmout != initialAmount + 50):
+        elif(i == 4 and currentAmout != initialAmount + 50):
             raise Exception(
                 '[fail]: the value returned is not expected in 5 action')
 
@@ -48,18 +48,6 @@ def checkDeposits(accountId):
             '[fail]: deposit not working')
 
 
-def tryDepositNegativeValues(accountId):
-    bankRepository = BankRepository(clientAuthKey)
-    initialAmount = bankRepository.getBalance(accountId)
-    for i in range(5):
-        bankRepository.deposit(accountId, -10)
-
-    currentAmout = bankRepository.getBalance(accountId)
-    if(currentAmout != initialAmount):
-        raise Exception(
-            '[fail]: deposit is working with negative values')
-
-
 def checkWithdrawal(accountId):
     bankRepository = BankRepository(clientAuthKey)
     bankRepository.deposit(accountId, 40)
@@ -75,34 +63,22 @@ def checkWithdrawal(accountId):
 
 def checkWithdrawal(accountId):
     bankRepository = BankRepository(clientAuthKey)
-    bankRepository.deposit(accountId, 40)
     initialAmount = bankRepository.getBalance(accountId)
+    bankRepository.deposit(accountId, 40)
     for i in range(4):
         bankRepository.withdrawal(accountId, 10)
 
     currentAmount = bankRepository.getBalance(accountId)
-    if(currentAmount != initialAmount - 40):
+    if(currentAmount != initialAmount):
         raise Exception(
             '[fail]: withdrawal not working')
-
-
-def tryWithdrawalNegativeValues(accountId):
-    bankRepository = BankRepository(clientAuthKey)
-    initialAmount = bankRepository.getBalance(accountId)
-    for i in range(5):
-        bankRepository.withdrawal(accountId, -10)
-
-    currentAmout = bankRepository.getBalance(accountId)
-    if(currentAmout != initialAmount):
-        raise Exception(
-            '[fail]: withdrawal is working with negative values')
 
 
 def checkInvalidWithdrawal(accountId):
     bankRepository = BankRepository(clientAuthKey)
     initialAmount = bankRepository.getBalance(accountId)
     for i in range(5):
-        bankRepository.withdrawal(accountId, initialAmount + 1)
+        bankRepository.withdrawal(accountId, int(initialAmount + 1))
 
     currentAmount = bankRepository.getBalance(accountId)
     if(currentAmount != initialAmount or currentAmount < 0):
@@ -112,9 +88,9 @@ def checkInvalidWithdrawal(accountId):
 
 def checkTransfer(accountId):
     bankRepository = BankRepository(clientAuthKey)
-    bankRepository.deposit(40, accountId)
     initialAmountOrigin = bankRepository.getBalance(accountId)
     initialAmountTarget = bankRepository.getBalance(accountId + 1)
+    bankRepository.deposit(accountId, 40)
 
     for i in range(4):
         bankRepository.transfer(accountId, accountId + 1, 10)
@@ -122,7 +98,7 @@ def checkTransfer(accountId):
     currentAmountOrigin = bankRepository.getBalance(accountId)
     currentAmountTarget = bankRepository.getBalance(accountId + 1)
 
-    if(initialAmountOrigin != currentAmountOrigin - 40 or initialAmountTarget != currentAmountTarget + 40):
+    if(initialAmountOrigin != currentAmountOrigin or initialAmountTarget + 40 != currentAmountTarget):
         raise Exception(
             '[fail]: transfer not working')
 
@@ -133,7 +109,7 @@ def tryToTransferMoreThanOriginHad(accountId):
     initialAmountTarget = bankRepository.getBalance(accountId + 1)
     for i in range(5):
         bankRepository.transfer(accountId, accountId +
-                                1, initialAmountOrigin + 1)
+                                1, int(initialAmountOrigin + 1))
 
     currentAmountOrigin = bankRepository.getBalance(accountId)
     currentAmountTarget = bankRepository.getBalance(accountId + 1)
@@ -142,28 +118,25 @@ def tryToTransferMoreThanOriginHad(accountId):
         raise Exception(
             '[fail]: unexpect transfer')
 
+def concurrentRequests(accountId):
 
-def __init__():
-    accountId = 1
 
-    # auth test
-    printTestResult(tryRequestWithoutAuthenticaiton,
-                    accountId, '[success]: authentication is necessary')
+accountId = 1
 
-    # operations test
+# auth test
+printTestResult(tryRequestWithoutAuthenticaiton,
+                accountId, '[success]: authentication is necessary')
 
-    printTestResult(checkIfOnlyIn5ActionSaveInformations, accountId,
-                    '[success]: in check if only in 5 action the business api would save the information')
-    printTestResult(checkDeposits, accountId, '[success]: deposit works')
-    printTestResult(tryDepositNegativeValues, accountId,
-                    '[success]: deposit not work with negative values')
-    printTestResult(checkWithdrawal, accountId, '[success]: withdrawal works')
-    printTestResult(checkInvalidWithdrawal, accountId,
-                    '[success]: limit in withdrawal works')
-    printTestResult(tryWithdrawalNegativeValues, accountId,
-                    '[success]: withdrawal not wotk with negative values')
-    printTestResult(checkTransfer, accountId, '[success]: transfer works')
-    printTestResult(tryToTransferMoreThanOriginHad, accountId,
-                    '[success]: not transfer if origin not have enough amount')
+# operations test
 
-    # concurrency test
+printTestResult(checkIfOnlyIn5ActionSaveInformations, accountId,
+                '[success]: in check if only in 5 action the business api would save the information')
+printTestResult(checkDeposits, accountId, '[success]: deposit works')
+printTestResult(checkWithdrawal, accountId, '[success]: withdrawal works')
+printTestResult(checkInvalidWithdrawal, accountId,
+                '[success]: limit in withdrawal works')
+printTestResult(checkTransfer, accountId, '[success]: transfer works')
+printTestResult(tryToTransferMoreThanOriginHad, accountId,
+                '[success]: not transfer if origin not have enough amount')
+
+# concurrency test
